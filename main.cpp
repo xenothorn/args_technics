2f545f0771642034a952cf8903cfbf58b315c252
Nickolay <xenothorn@yandex.ru>
2023-07-30 13:15:29 +0300

    resolve

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -21,11 +22,62 @@ TokenForwardIt FindSentenceEnd(TokenForwardIt tokens_begin, TokenForwardIt token
           });
     return before_sentence_end == tokens_end ? tokens_end : next(before_sentence_end);
 }
-
+/*
 // Класс Token имеет метод bool IsEndSentencePunctuation() const
 template <typename Token>
 vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens) {
     // Напишите реализацию функции, не копируя объекты типа Token
+    vector<Sentence<Token>> a;
+    vector<Token> tv;
+    bool end_of_sentence = false;
+    for (const auto& t: tokens)
+    {
+        if (t.IsEndSentencePunctuation()) 
+        {
+            if (!end_of_sentence) end_of_sentence = true;
+            tv.push_back(move(t));
+        }
+        else
+        {
+            if (end_of_sentence)
+            {
+                end_of_sentence = false;
+                a.push_back(move(tv));
+                tv.clear();            
+            }
+            tv.push_back(move(t));  
+        }
+    }
+    a.push_back(move(tv));
+    return a;
+}*/
+// Класс Token имеет метод bool IsEndSentencePunctuation() const
+template <typename Token>
+vector<Sentence<Token>> SplitIntoSentences(vector<Token>&& tokens) {
+    // Напишите реализацию функции, не копируя объекты типа Token
+    vector<Sentence<Token>> a;
+    vector<Token> tv;
+    bool end_of_sentence = false;
+    for (auto it = tokens.begin(); it != tokens.end(); ++it)
+    {
+        if ((*it).IsEndSentencePunctuation()) 
+        {
+            if (!end_of_sentence) end_of_sentence = true;
+            tv.push_back(move(*it));
+        }
+        else
+        {
+            if (end_of_sentence)
+            {
+                end_of_sentence = false;
+                a.push_back(move(tv));
+                tv.clear();            
+            }
+            tv.push_back(move(*it));  
+        }
+    }
+    a.push_back(move(tv));
+    return a;
 }
 
 struct TestToken {
@@ -40,7 +92,16 @@ struct TestToken {
     }
 };
 
+struct TestTokenNoCopy :public TestToken {
+    TestTokenNoCopy(const TestTokenNoCopy& other) = delete;
+    TestTokenNoCopy(TestTokenNoCopy&& other) = default;
+};
+
 ostream& operator<<(ostream& stream, const TestToken& token) {
+    return stream << token.data;
+}
+
+ostream& operator<<(ostream& stream, const TestTokenNoCopy& token) {
     return stream << token.data;
 }
 
@@ -62,6 +123,24 @@ void TestSplitting() {
            }));
 }
 
+
+void TestSplittingNoCopy(){
+    vector<TestTokenNoCopy> nct;
+    nct.push_back({"send"s});
+    nct.push_back({"send"s});
+    nct.push_back({"."s, true});
+
+    vector<TestTokenNoCopy> ncc;
+    ncc.push_back({"send"s});
+    ncc.push_back({"send"s});
+    ncc.push_back({"."s, true});
+
+    vector<Sentence<TestTokenNoCopy>> st = move(SplitIntoSentences(move(nct)));
+    assert (st[0] == ncc) ;
+}
+
+
 int main() {
+    TestSplittingNoCopy();
     TestSplitting();
 }
